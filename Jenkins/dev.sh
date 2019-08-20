6990064f1e3dad2f311a0affe76e4424a6d35633
ktg0210 <31715553+ktg0210@users.noreply.github.com>
2019-08-20 14:23:32 +0900

    Update dev.sh

--- Jenkins/dev.sh
+++ Jenkins/dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-sudo scp -i /home/user/.ssh/id_rsa -P port /var/lib/jenkins/workspace/project/target/*jar dev@#.#.#.#: 1> /dev/null 2>&1
+# copy jar file to ec2 server for stage 
+sudo scp -i /home/user/.ssh/id_rsa -P port /var/lib/jenkins/workspace/project/target/*jar dev@x.x.x.x: 1> /dev/null 2>&1
 sleep 1
-sudo ssh  -i /home/user/.ssh/id_rsa -p port dev@#.#.#.# sh ./start.sh
+# excute start shell on ec2 server for stage
+sudo ssh  -i /home/user/.ssh/id_rsa -p port dev@x.x.x.x sh ./start.sh
